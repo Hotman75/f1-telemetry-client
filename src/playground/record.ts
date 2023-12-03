56f1c8d9d291dd13ca4b06d0f5132abd5a2fbe44
Hotman75 <othman.amor@gmail.com>
2023-12-03 17:13:03 +0100

    txt to json

--- src/playground/record.ts
+++ src/playground/record.ts
@@ -33,8 +33,8 @@ client.on('raw', ({packetData, packetID, message}) => {
       (key, value) => (typeof value === 'bigint' ? value.toString() : value)
     ) + '\n';
 
-  fs.writeFileSync(`./recordings/${filename}-${packetID}.txt`, data);
-  fs.writeFileSync(`./recordings/${filename}-all.txt`, data);
+  fs.writeFileSync(`./recordings/${filename}-${packetID}.json`, data);
+  fs.writeFileSync(`./recordings/${filename}-all.json`, data);
 });
 
 client.start();
